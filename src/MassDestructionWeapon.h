72cc68fac3c877287b10df5a0c97758e1abe080b
tahakhurram98 <56072999+tahakhurram98@users.noreply.github.com>
2019-11-12 17:04:33 -0500

    Add files via upload

---
+++ src/MassDestructionWeapon.h
@@ -0,0 +1,31 @@
+#ifndef __MASSDESTRUCTIONWEAPON_H
+#define __MASSDESTRUCTIONWEAPON_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+
+#include "Weapon.h"
+#include "Point.h"
+#include "Enemy.h"
+#include <math.h>
+
+class MassDestructionWeapon : public Weapon{
+    public:
+        MassDestructionWeapon(int damageValue, int radiusValue);
+        ~MassDestructionWeapon();
+	
+	void destroy(Enemy* enemyList, Point blastCenter, int listLength);
+	//setter methods
+	void setDamage(int damageValue);
+	void setBlastRadius(int radiusValue);
+	//getter methods
+	int getDamage();
+	int getBlastRadius();
+
+    private:
+        int damage;
+	int blastRadius;
+};
+
+#endif
---
+++ src/Weapon.cpp
@@ -0,0 +1,11 @@
+#include "Weapon.h"
+
+Weapon::Weapon(){
+//no attributes to initialize
+}
+
+Weapon::Weapon(){
+//no memory allocation to delete
+}
+
+}
